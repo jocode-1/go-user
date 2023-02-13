673de13b9641df97b6d557b2e4e2c584f84d14c7
jocode-1 <akintolajohn41@gmail.com>
2023-02-13 16:05:43 +0100

    Update User

--- services/userServImplem.go
+++ services/userServImplem.go
@@ -2,8 +2,11 @@ package services
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/jocode-1/go-user/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -29,8 +32,10 @@ func (u *UserServiceImpl) CreateUser(user *models.User) error {
 func (u *UserServiceImpl) GetUser(id *int) (*models.User, error) {
 
 	var user *models.User
+	query := bson.D{bson.E{Key: "id", Value: id}}
+	err := u.usercollection.FindOne(u.ctx, query).Decode(&user)
 
-	return nil, nil
+	return user, err
 }
 
 func (u *UserServiceImpl) GetAll() ([]*models.User, error) {
@@ -39,6 +44,15 @@ func (u *UserServiceImpl) GetAll() ([]*models.User, error) {
 }
 
 func (u *UserServiceImpl) UpdateUser(user *models.User) error {
+
+	filter := bson.D{bson.E{Key: "id", Value: user.Id}}
+	update := bson.D{bson.E{Key: "id", Value: bson.D{bson.E{Key: "id", Value: user.Id}, bson.E{Key: "name", Value: user.Name}, bson.E{Key: "email", Value: user.Email}, bson.E{Key: "address", Value: user.Address}}}
+
+	result, _ := u.usercollection.UpdateOne(u.ctx, filter, update)
+	if result.MatchedCount != 1 {
+		return fmt.Sprintf(errors.New("No User Found By%s", filter)) 
+	}
+
 
 	return nil
 }
